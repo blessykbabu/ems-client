9172624da03a2e7270bf81420ce9c4a696b9cf3d
blessykbabu <blessykbabu5@gmail.com>
2024-01-10 17:25:49 +0530

    client

--- client/src/components/AdminDasboard.jsx
+++ client/src/components/AdminDasboard.jsx
@@ -7,9 +7,6 @@
 // import EmployeeProfileComponent from './EmployeeProfileComponent';
 // import UpdateComponent from './updateComponent';
 
-
-
-
 // export default function  AdminDasboard(){
 // return(
 //    <>
@@ -21,7 +18,7 @@
 //             <Route path='/view' element={<EmployeeProfileComponent/>}/>
 //             <Route path='/profile/:id' element={<UpdateComponent/>}/>
 //         </Routes>
-        
+
 //       <div className='home'>
 
 //       </div>
@@ -29,44 +26,60 @@
 // )
 // }
 
-
-
 // AdminDashboard.jsx
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-import RegFormComponent from './RegFormComponent';
-
-
+import RegFormComponent from "./RegFormComponent";
 
 export default function AdminDasboard() {
   return (
-
-<>
-    <nav className="navbar navbar-expand-lg bg-dark">
-  <div className="container-fluid">
-   <Link to="/" style={{textDecoration:"none",color:"white"}}>Home</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-     
-        <li className="nav-item m-4">
-        <Link to="/registration" style={{textDecoration:"none",color:"white"}}>Registration</Link>
-        </li>
-        <li className="nav-item m-4">
-       <Link to="/view" style={{textDecoration:"none",color:"white"}}>Employee List</Link>
-        </li>
-        <li className="nav-item m-4">
-       <Link to="/logout" style={{textDecoration:"none",color:"white"}}>Logout</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-   
-   
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            Home
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item m-4">
+                <Link
+                  to="/registration"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Registration
+                </Link>
+              </li>
+              <li className="nav-item m-4">
+                <Link
+                  to="/view"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Employee List
+                </Link>
+              </li>
+              <li className="nav-item m-4">
+                <Link
+                  to="/logout"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
